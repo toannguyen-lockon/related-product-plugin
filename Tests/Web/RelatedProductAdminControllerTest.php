70fa07d2ea531d712ef8e97ed3206192d16eace3
tona-lc <nvtoan@lockon-vn.com>
2017-11-06 11:37:06 +0700

    Add to Travis

--- Tests/Web/RelatedProductAdminControllerTest.php
+++ Tests/Web/RelatedProductAdminControllerTest.php
@@ -43,6 +43,7 @@ class RelatedProductAdminControllerTest extends AbstractAdminWebTestCase
      */
     public function testCreateRelatedProduct()
     {
+
         $faker = $this->getFaker();
         $content = $faker->word;
         $childProductId = 1;
